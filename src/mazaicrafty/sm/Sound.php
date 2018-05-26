1b72f111264f3c97123c3efa2dd8b86c25c0e934
Lamda <takatim1574@icloud.com>
2018-05-26 15:52:40 +0900

    Add files via upload

---
+++ src/mazaicrafty/sm/Sound.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* The MIT License
+* Copyright (c) 2018 MazaiCrafty
+*/
+
+namespace mazaicrafty\sm;
+
+class Sound{
+    const ANVIL_BREAK = 1;
+    const ANVIL_FALL = 2;
+    const ANVIL_USE = 3;
+    const BAT = 4;
+    const BLAZESHOOT = 5;
+    const CLICK = 6;
+    const DOOR_BUMP = 7;
+    const DOOR_CRASH = 8;
+    const ENDERMAN_TELEPORT = 9;
+    const FIZZ = 10;
+    const GHAST_SHOOT = 11;
+    const GHAST = 12;
+    const LAUNCH = 13;
+    const POP = 14;
+    const GENERIC = 15;
+}
